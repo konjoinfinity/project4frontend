9001c42188ec0a81f12de3725c0bd2fa17dd6a98
Wesley Scholl <clients@konjoweb.com>
2019-04-08 13:15:21 -0400

    added signup login logout links and bootstrap cdn

--- project4/src/App.js
+++ project4/src/App.js
@@ -1,11 +1,147 @@
 import React, { Component } from "react";
+import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
+import Signup from "./Signup";
+import Login from "./Login";
+import Logout from "./Logout";
+import backendUrl from "./Url";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      isLoggedIn: false
+    };
+    this.handleLogOut = this.handleLogOut.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.handleLogIn = this.handleLogIn.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  componentDidMount() {
+    if (localStorage.token) {
+      this.setState({
+        isLoggedIn: true
+      });
+    } else {
+      this.setState({
+        isLoggedIn: false
+      });
+    }
+  }
+
+  handleLogOut(e) {
+    e.preventDefault();
+    this.setState({
+      email: "",
+      password: "",
+      isLoggedIn: false
+    });
+    localStorage.clear();
+    console.log("User has been logged out");
+    this.props.history.push("/login");
+  }
+
+  handleInput(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSignUp(e) {
+    e.preventDefault();
+    axios
+      .post(backendUrl + "users/signup", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        localStorage.token = response.data.token;
+        this.setState({ isLoggedIn: true });
+        console.log("User has signed up");
+        this.props.history.push("/songs");
+      })
+      .catch(err => console.log(err));
+  }
+
+  handleLogIn(e) {
+    e.preventDefault();
+    axios
+      .post(backendUrl + "users/login", {
+        email: this.state.email,
+        password: this.state.password
+      })
+      .then(response => {
+        localStorage.token = response.data.token;
+        this.setState({ isLoggedIn: true });
+        console.log("User is logged in");
+        this.props.history.push("/songs");
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Konjo</h1>
+        <nav className="navbar navbar-expand-lg navbar-light bg-success">
+          <a className="navbar-brand" href="/">
+            <img
+              src="https://konjoinfinity.github.io/img/logo.png"
+              alt="Konjo"
+            />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarText"
+            aria-controls="navbarText"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav mr-auto">
+              <li
+                className="nav-item active"
+                data-toggle="collapse"
+                data-target=".navbar-collapse.show"
+              >
+                {this.state.isLoggedIn === false && (
+                  <Link to="/login">
+                    <h4>Login</h4>
+                  </Link>
+                )}
+              </li>
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target=".navbar-collapse.show"
+              >
+                {this.state.isLoggedIn === false && (
+                  <Link to="/signup">
+                    <h4>Sign Up</h4>
+                  </Link>
+                )}
+              </li>
+              <li
+                className="nav-item"
+                data-toggle="collapse"
+                data-target=".navbar-collapse.show"
+              >
+                {this.state.isLoggedIn === true && (
+                  <Link to="/logout">
+                    <h4>Logout</h4>
+                  </Link>
+                )}
+              </li>
+            </ul>
+            <span className="navbar-text">Konjo Communities</span>
+          </div>
+        </nav>
       </div>
     );
   }
